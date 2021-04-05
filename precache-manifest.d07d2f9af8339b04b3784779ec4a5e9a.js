8217386c9d3dbbeefae75bba431df09d15dac0b8
unknown <shuklaaalind.com>
2021-04-06 00:29:32 +0530

    Updates

---
+++ precache-manifest.d07d2f9af8339b04b3784779ec4a5e9a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd65f005558f5803ab2903302d089963",
+    "url": "/Todo-App/index.html"
+  },
+  {
+    "revision": "667c339085260eab67b0",
+    "url": "/Todo-App/static/css/main.3abff1a8.chunk.css"
+  },
+  {
+    "revision": "27130051255a819fa7ea",
+    "url": "/Todo-App/static/js/2.7bd08d42.chunk.js"
+  },
+  {
+    "revision": "667c339085260eab67b0",
+    "url": "/Todo-App/static/js/main.3d0c7dec.chunk.js"
+  },
+  {
+    "revision": "2acd21dc4a6582beab29",
+    "url": "/Todo-App/static/js/runtime~main.3fb21290.js"
+  }
+]);
